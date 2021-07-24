12ce30088d459e35b0b2d4d93107ab8f5fba3c00
Srikanth Gubba <gubbasrikanth2355@gmail.com>
2021-07-24 07:20:28 +0530

    added components like user,button,card,adduser. used css modules with input validation

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import Card from "../UI/Card";
+import Button from '../UI/Button'
+import classes from "./AddUser.module.css"
+
+function AddUser(props) {
+    const [enteredusername, setenteredusername] = useState('');
+    const [enteredage, setenteredage] = useState('');
+    const adduserHandler = (event) => {
+        event.preventDefault();
+        if (enteredusername.trim().length === 0 || enteredage.trim().length === 0) {
+            return;
+        }
+        if (+enteredage < 5) { //+ converts string into number
+            return;
+        }
+        setenteredusername('');
+        setenteredage('');
+    }
+    const usernamechangeHandler = (event) => {
+        setenteredusername(event.target.value);
+    }
+    const agechangeHandler = (event) => {
+        setenteredage(event.target.value);
+    }
+    return (
+        <Card className={classes.input}>
+            <form   onSubmit={adduserHandler}>
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" value={enteredusername} onChange={usernamechangeHandler} />
+                <label htmlFor="age">Age</label>
+                <input type="number" id="age" value={enteredage} onChange={agechangeHandler} />
+                <Button type="submit">Add User</Button>
+            </form> 
+        </Card>
+                   
+    )
+}
+
+export default AddUser
