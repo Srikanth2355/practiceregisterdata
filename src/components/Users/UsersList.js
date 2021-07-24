cc7ee0344793d7d2fc73437ea541feed56769e2b
Srikanth Gubba <gubbasrikanth2355@gmail.com>
2021-07-24 07:36:59 +0530

    corrected a typo error in userlist.js file

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -4,7 +4,7 @@ import classes from "./UsersList.module.css"
 
 function UsersList(props) {
     return (
-        <Card classname={classes.users}>
+        <Card className={classes.users}>
             <ul>
                 {props.users.map((user) => {
                     return (  
